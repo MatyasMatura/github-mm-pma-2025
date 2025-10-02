1d5741e9968cecf99baa3efa1a380727f1aafebc
MatyasMatura <matyas182@gmail.com>
2025-10-02 12:07:03 +0200

    add input fields and buttons for team information submission in MainActivity

--- CvikoApp02MyConstraintLayout/app/src/main/java/com/example/cvikoapp02myconstraintlayout/MainActivity.kt
+++ CvikoApp02MyConstraintLayout/app/src/main/java/com/example/cvikoapp02myconstraintlayout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cvikoapp02myconstraintlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,38 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+        val etName = findViewById<EditText>(R.id.etTeamName)
+        val etLeague = findViewById<EditText>(R.id.etTeamLeague)
+        val etCountry = findViewById<EditText>(R.id.etTeamCountry)
+        val etCity = findViewById<EditText>(R.id.etTeamCity)
+        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+
+        // Nastavení obsluhy tlačítka "Odeslat"
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val country = etCountry.text.toString()
+            val league = etLeague.text.toString()
+            val city = etCity.text.toString()
+            val info = "Jméno: $name\nZemě: $country\nLiga: $league\nMěsto: $city"
+            tvInfo.text = info
+        }
+
+        // Nastavení obsluhy tlačítka "Smazat"
+        btnDelete.setOnClickListener {
+            etName.text.clear()
+            etCountry.text.clear()
+            etLeague.text.clear()
+            etCity.text.clear()
+            tvInfo.text = ""
         }
     }
 }
