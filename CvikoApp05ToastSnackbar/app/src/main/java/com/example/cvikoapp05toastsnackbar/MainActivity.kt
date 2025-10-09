6ac95368e88893ed14569ecb09aa78792435a34d
MatyasMatura <matyas182@gmail.com>
2025-10-09 12:03:05 +0200

    refactor layout and add Snackbar and Toast functionality in MainActivity

--- CvikoApp05ToastSnackbar/app/src/main/java/com/example/cvikoapp05toastsnackbar/MainActivity.kt
+++ CvikoApp05ToastSnackbar/app/src/main/java/com/example/cvikoapp05toastsnackbar/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.cvikoapp05toastsnackbar
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cvikoapp05toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSnackbar.setOnClickListener {
+
+            Snackbar.make(binding.root, "This is a Snackbar", Snackbar.LENGTH_LONG)
+                .setBackgroundTint(Color.parseColor("#FFBB86FC"))
+                .setTextColor(Color.BLUE)
+                .setDuration(7000)
+                .setActionTextColor(Color.WHITE)
+                .setAction("Zavřít") {
+                    Toast.makeText(this, "Snackbar zavřen", Toast.LENGTH_LONG)
+                        .show()
+                }
+                .show()
+        }
+
+        binding.btnToast.setOnClickListener {
+
+            Toast.makeText(this, "This is a Toast", Toast.LENGTH_LONG)
+                .show()
         }
     }
 }
