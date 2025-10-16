c7644a68264ff881f31570cd9e67de30a7b88b02
MatyasMatura <matyas182@gmail.com>
2025-10-16 11:56:29 +0200

    add fragments and update MainActivity to handle fragment navigation

--- CvikoApp07SharedPreferences/app/src/main/java/com/example/cvikoapp07sharedpreferences/MainActivity.kt
+++ CvikoApp07SharedPreferences/app/src/main/java/com/example/cvikoapp07sharedpreferences/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.cvikoapp07sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+            binding.tvDeviceName.text = "Notebook"
         }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+            binding.tvDeviceName.text = "Desktop PC"
+        }
+
+        binding.btnFragment3.setOnClickListener {
+            replaceFragment(Fragment3())
+            binding.tvDeviceName.text = "Tablet"
+        }
+
+        binding.btnFragment4.setOnClickListener {
+            replaceFragment(Fragment4())
+            binding.tvDeviceName.text = "Telefon"
+        }
+
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
