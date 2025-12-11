1dc31ff1ceb3baa403307a34f84cd42529c696d3
MatyasMatura <matyas182@gmail.com>
2025-12-11 11:47:25 +0100

    Ukol 16 - vizualizace pro drawe menu + ikonky

--- CvikoApp15Navigation/app/src/main/java/com/example/cvikoapp15navigation/MainActivity.kt
+++ CvikoApp15Navigation/app/src/main/java/com/example/cvikoapp15navigation/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.cvikoapp15navigation
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,6 +14,8 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -31,9 +33,23 @@ class MainActivity : AppCompatActivity() {
         // Nastavíme ho jako ActionBar
         setSupportActionBar(toolbar)
 
-        // Až teď může NavigationUI pracovat s ActionBarem
-        setupActionBarWithNavController(navController)
+        // Najdeme DrawerLayout
+        drawerLayout = findViewById(R.id.drawerLayout)
 
+        // Vytvoříme ActionBarDrawerToggle pro hamburger menu
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+
+        // Přidáme listener pro drawer
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        // Propojíme navigace s menu
         findViewById<BottomNavigationView>(R.id.bottomNav)
             .setupWithNavController(navController)
 
